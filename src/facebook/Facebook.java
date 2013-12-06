71d6da847991bec319144c7693015ab72e41d7c9
Olga Shestopalova <olgash@mit.edu>
2013-12-06 16:56:43 -0500

    ASKDJHASDKJHD

--- src/facebook/Facebook.java
+++ src/facebook/Facebook.java
@@ -1,10 +1,10 @@
 package facebook;
 
-import com.restfb.BinaryAttachment;
-import com.restfb.DefaultFacebookClient;
-import com.restfb.FacebookClient;
-import com.restfb.Parameter;
-import com.restfb.types.FacebookType;
+import restfb.BinaryAttachment;
+import restfb.DefaultFacebookClient;
+import restfb.FacebookClient;
+import restfb.Parameter;
+import restfb.types.FacebookType;
 
 /**
  * Finds Olga's facebook. Use this to post images.
