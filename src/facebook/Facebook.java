c9bea0b7945011dccc74d7c846c29cdbc85e7482
Olga Shestopalova <olgash@mit.edu>
2013-12-07 13:56:29 -0500

    EVERYONE GO PRESS THE FACEBOOK BUTTON

--- src/facebook/Facebook.java
+++ src/facebook/Facebook.java
@@ -1,6 +1,5 @@
 package facebook;
 
-import restfb.BinaryAttachment;
 import restfb.DefaultFacebookClient;
 import restfb.FacebookClient;
 import restfb.Parameter;
@@ -12,9 +11,12 @@ import restfb.types.FacebookType;
  * TODO: we should really not be posting to my facebook....figure out this OAUTH
  * shit
  * 
+ * access token:
+ * CAADvFymPHnABAAeFmmKZBckkarf0Sc0BHZCaMpRcrgpScnzdWFtZCOYEcqkBrOg6fhgyQieTU8WrcPcKGquJcXEOtIaA5AlAZAG9t4V1IdeetqNZCJbhBd1fR731sbYQDoBXpgGl6lAZA1KDf6yWHIFZBtJrohvUV6Wyb3LYezAsN5PFAqSaPar
+ * 
  */
 public class Facebook {
-    private static final String MY_ACCESS_TOKEN = "CAACEdEose0cBANYtS1nkNJMH8wei0OW1ixro4nXI7FPbXzoOMIm3Q38BB1mGDduKzdhqOssq7zzP4Q35RdvZAMK27codbd3ALd9sgfOKslRZB0xPCRpRMKRV3F0desSZB5sePgUcbMP9XbZAxaZCtQJhGOHAXoooxF8VP7AuKAhj5ofaGc7sCrxLsRtgka8kZD";
+    private static final String MY_ACCESS_TOKEN = "CAADvFymPHnABAAeFmmKZBckkarf0Sc0BHZCaMpRcrgpScnzdWFtZCOYEcqkBrOg6fhgyQieTU8WrcPcKGquJcXEOtIaA5AlAZAG9t4V1IdeetqNZCJbhBd1fR731sbYQDoBXpgGl6lAZA1KDf6yWHIFZBtJrohvUV6Wyb3LYezAsN5PFAqSaPar";
 
     FacebookClient facebookClient = new DefaultFacebookClient(MY_ACCESS_TOKEN);
 
@@ -26,10 +28,18 @@ public class Facebook {
      *            name of image, including extension
      */
     public void publishImage(String name) {
-        FacebookType publishPhotoResponse = facebookClient.publish("images/", FacebookType.class,
-                BinaryAttachment.with(name, getClass().getResourceAsStream("/" + name)),
-                Parameter.with("message", "005 proj test!"));
+        // FacebookType publishPhotoResponse = facebookClient.publish("images/",
+        // FacebookType.class,
+        // BinaryAttachment.with(name, getClass().getResourceAsStream("/" +
+        // name)),
+        // Parameter.with("message", "005 proj test!"));
+        //
+        // System.out.println("Published photo ID: " +
+        // publishPhotoResponse.getId());
 
-        System.out.println("Published photo ID: " + publishPhotoResponse.getId());
+        FacebookType publishMessageResponse =
+                facebookClient.publish("me/feed", FacebookType.class,
+                        Parameter.with("message", "RestFB test"));
+
     }
 }
