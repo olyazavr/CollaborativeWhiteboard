4e77b04d8bb1097e1b43a300b0e305182acd606b
Olga Shestopalova <olgash@mit.edu>
2013-12-08 21:53:27 -0500

    BUTTONS ARE BEAUTIFUL

--- src/client/Canvas.java
+++ src/client/Canvas.java
@@ -396,10 +396,6 @@ public class Canvas extends JPanel {
         for (final Entry<JButton, Color> button : colorButtons.entrySet()) {
             // set the bg color of the button
             button.getKey().setBackground(button.getValue());
-            // remove the default buttons
-            button.getKey().setBorderPainted(false);
-            // make the background visible
-            button.getKey().setOpaque(true);
 
             // adding components to the pallet
             colorPallet.add(button.getKey());
